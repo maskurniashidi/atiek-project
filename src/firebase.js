43e7f56448cfb794f050e9da5fbf711cad9adf81
maskurniashidi <maskurshidi12@gmail.com>
2025-05-28 11:38:40 +0700

    setup firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3pRCr15lDSMIy2H3t1jb3WVejQNc7nlI",
+  authDomain: "atiek-project.firebaseapp.com",
+  projectId: "atiek-project",
+  storageBucket: "atiek-project.firebasestorage.app",
+  messagingSenderId: "727894881383",
+  appId: "1:727894881383:web:8568fca4c1196bf4d0d0c2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
